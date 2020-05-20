77b7539f67cbd706386ad25d8b7adada922cf9bc
gabriel <gabo.lazos@gmail.com>
2020-05-20 11:11:33 -0500

    adding desktop files

---
+++ matrix.bat
@@ -0,0 +1,10 @@
+@echo off
+title Bucle de ejemplo
+color 0A
+
+:frases
+echo 100101001001010010010 Creado por Juanma 100101001001010010010 
+echo 011010110110101101101 Creado por Juanma 011010110110101101101 
+
+goto frases
+exit
---
+++ miPrimerPrograma.bat
@@ -0,0 +1,14 @@
+@echo off
+title Hello world
+color E0
+set/p nombre=Cual es tu nombre? 
+set/p apellido=Cual es tu apellido? 
+echo calculando la probabilidad de que tu nombre sea "%nombre% %apellido%"
+pause
+echo 100 porciento probable.
+echo liberando recursos de computo y memoria
+pause
+exit
+
+
+
